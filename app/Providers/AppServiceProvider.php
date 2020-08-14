43ab062db43ca3e3e1ab30dee95a30bdd0ba8b3d
asano <asano@localhost.localdomain>
2020-08-14 14:56:48 +0900

    first commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+     //
+     // 以下を追記
+     if (\App::environment('production')) {
+        \URL::forceScheme('https');
+     }
+
     }
+
 }
